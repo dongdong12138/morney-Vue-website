1c8eb2b17094858d5707ca31264cab1dddd024d0
董浩 <barry.dong@morefun.me>
2021-12-12 11:13:35 +0800

    deploy

---
+++ precache-manifest.b44971bd3306eebd688dfc4a082194de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2acea528d57814db9c8",
+    "url": "/morney-Vue-website/css/app.ce280974.css"
+  },
+  {
+    "revision": "0a4c4794c5ea73339a11d11874700afe",
+    "url": "/morney-Vue-website/index.html"
+  },
+  {
+    "revision": "a2acea528d57814db9c8",
+    "url": "/morney-Vue-website/js/app.da2a848d.js"
+  },
+  {
+    "revision": "ad35d399310c9e339bea",
+    "url": "/morney-Vue-website/js/chunk-vendors.284fd057.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-Vue-website/manifest.json"
+  },
+  {
+    "revision": "073c57c468160345fe16d8b7e18c71ee",
+    "url": "/morney-Vue-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-Vue-website/robots.txt"
+  }
+]);
